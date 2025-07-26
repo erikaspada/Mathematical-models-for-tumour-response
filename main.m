54ca259de7cb796a7daa716fe6c28c1723969cb9
erikaspada <erika372000@gmail.com>
2025-07-26 19:16:17 +0200

    Add files via upload

---
+++ main.m
@@ -0,0 +1,16 @@
+%% Main script 
+
+% Load parameters and initial conditions
+[params, state] = define_parameters();
+
+% Generate drug administration profile
+u1 = drug_schedule(params, 'standard');
+
+% Solve PDE model
+[state, time, x] = solve_pde(params, state, u1);
+
+% Solve Euler system (f_e, mu_e, rho_e)
+[euler_out] = euler_solver(params, u1);
+
+% Plot and compare results
+plot_results(params, state, euler_out, time, x, u1);
